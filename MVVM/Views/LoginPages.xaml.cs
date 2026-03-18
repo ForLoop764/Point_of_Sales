1012e3ad7655ae140869769c7b24e8d780226596
ACT-STUDENT <ACT-STUDENT@LAB2-PC30>
2026-03-18 17:14:59 +0800

    s

--- MVVM/Views/LoginPages.xaml.cs
+++ MVVM/Views/LoginPages.xaml.cs
@@ -2,22 +2,40 @@ using Point_of_Sales.MVVM.ViewModels;
 
 namespace Point_of_Sales.MVVM.Views;
 
-public partial class LoginPage : ContentPage  
+public partial class LoginPage : ContentPage
 {
+    private Login _viewModel;
+
     public LoginPage()
     {
         InitializeComponent();
-        BindingContext = new Login();
+        _viewModel = new Login();
+        BindingContext = _viewModel;
     }
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-        Application.Current.MainPage = new UserManagement();
+        string username = UsernameEntry.Text;
+        string password = PasswordEntry.Text;
+
+        if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+        {
+            DisplayAlert("Error", "Please enter your username and password.", "OK");
+            return;
+        }
+
+        if (username == "Admin" && password == "Admin123")
+        {
+            Application.Current.MainPage = new UserManagement();
+        }
+        else
+        {
+            DisplayAlert("Login Failed", "Invalid username or password.", "OK");
+        }
     }
 
     private void Button_Clicked_1(object sender, EventArgs e)
     {
         Application.Current.MainPage = new RegisterPage();
-
     }
 }
