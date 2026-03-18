5444086daeedb60022533002342e70340c4f3ea8
ACT-STUDENT <ACT-STUDENT@LAB2-PC30>
2026-03-18 17:32:23 +0800

    hjk

--- MVVM/Views/UserManagement.xaml.cs
+++ MVVM/Views/UserManagement.xaml.cs
@@ -2,8 +2,23 @@ namespace Point_of_Sales.MVVM.Views;
 
 public partial class UserManagement : ContentPage
 {
-	public UserManagement()
-	{
-		InitializeComponent();
-	}
+    public UserManagement()
+    {
+        InitializeComponent();
+    }
+
+    private void OnAddUserClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Add User", "Open Add User form here.", "OK");
+    }
+
+    private void OnEditUserClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Edit User", "Open Edit User form here.", "OK");
+    }
+
+    private void OnDeleteUserClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Delete User", "Are you sure you want to delete this user?", "Yes", "No");
+    }
 }
