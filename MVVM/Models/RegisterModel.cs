81b107520b720258118ac202bd7d1ce4efa2c992
ACT-STUDENT <ACT-STUDENT@LAB2-PC30>
2026-03-18 17:09:42 +0800

    sd

---
+++ MVVM/Models/RegisterModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point_of_Sales.MVVM.Models
+{
+    internal class RegisterModel
+    {
+    }
+}
